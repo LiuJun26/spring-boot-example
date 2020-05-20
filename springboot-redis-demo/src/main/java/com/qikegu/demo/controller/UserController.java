bc9718a9b227f88139c9a822269829cd654a370d
liujun <1992404176@qq.com>
2020-05-20 14:59:15 +0800

    redis 修改提交

--- springboot-redis-demo/src/main/java/com/qikegu/demo/controller/UserController.java
+++ springboot-redis-demo/src/main/java/com/qikegu/demo/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.qikegu.demo.controller;
 
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
 import javax.annotation.Resource;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSON;
 import com.qikegu.demo.model.User;
 import com.qikegu.demo.service.UserService;
 
@@ -35,23 +39,29 @@ public class UserController {
     }
     
     // 修改用户信息，测试删除缓存
-    @RequestMapping(value = "/{id}/change-nick", method = RequestMethod.POST, produces="application/json")
+    @RequestMapping(value = "/{id}/change-nick", method = RequestMethod.GET, produces="application/json")
     public User changeNickname(@PathVariable long id) throws Exception{
-        
-        String nick = "abc-" + Math.random();
-        User user = this.userService.updateUserNickname(id, nick);
-        
+    	String uid = "user-nick-name-"+id;
+        User user = (User)redis.opsForValue().get(uid);
+        if(user == null){
+        	System.out.println("**********来到这里说明没有从redis查询缓存数据***********");
+        	String nick = "abc-" + Math.random();
+        	user = this.userService.updateUserNickname(id, nick);
+        	redis.opsForValue().set(uid, user);
+        	redis.opsForValue().set("obj-json", JSON.toJSONString(user));
+        }
         return user;
     }
     
     // 使用RedisTemplate访问redis服务器
     @RequestMapping(value="/redis", method=RequestMethod.GET, produces="application/json")
     public String redis() throws Exception {
-        
+        String uuid = UUID.randomUUID().toString();
         // 设置键"project-name"，值"qikegu-springboot-redis-demo"
         redis.opsForValue().set("project-name", "qikegu-springboot-redis-demo");
-        String value = (String) redis.opsForValue().get("project-name");
-        
-        return value;
+        redis.opsForValue().set("project-expire", uuid, 30, TimeUnit.SECONDS);
+        String value = (String) redis.opsForValue().get("project-expire");
+        Long expire = redis.getExpire("project-expire");
+        return value +expire;
     }
 }
